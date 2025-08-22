c4a858af939bfef2b14bf120886c6faf4ddf1b76
Onelastcode05 <163396909+Onelastcode05@users.noreply.github.com>
2025-08-22 23:20:07 +0530

    Time: 6 ms (69.57%) | Memory: 77.9 MB (24.66%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,18 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        count[0] = 1;   
-        int prefix = 0, ans = 0;
-
-        for (int x : nums) {
-            prefix += (x % 2);     
-            ans += count[prefix - k]; 
-            count[prefix]++;
+    int findOdd(vector<int> nums,int goal)
+    {
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        int count=0;
+        int sub=0;
+        while(r<n)
+        {
+            if(nums[r]%2!=0)
+            {
+                count++;
+            }
+            while(l<n && count>goal)
+            {
+                if(nums[l]%2!=0)
+                {
+                    count--;
+                }
+                l++;
+            }
+            sub+=(r-l+1);
+            r++;
         }
-        return ans;
+        return sub;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return findOdd(nums,k)-findOdd(nums,k-1);
     }
 };
