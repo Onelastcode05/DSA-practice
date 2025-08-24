09450406c8c72497634255799bfad1cb3e816054
Onelastcode05 <163396909+Onelastcode05@users.noreply.github.com>
2025-08-24 23:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (38.88%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n=bills.size();
+        int five=0;
+        int ten=0;
+        for(int bill:bills){
+            if(bill==5){
+                five++;
+            }
+            else if(bill==10){
+               if(five==0)return false;
+                five-=1;
+                ten+=1;
+            }
+            else{
+                if(five>0 && ten >0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five -=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
