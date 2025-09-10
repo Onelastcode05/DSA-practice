7220a32e06e26b30999847718e444de9f6cf6313
Onelastcode05 <163396909+Onelastcode05@users.noreply.github.com>
2025-09-10 09:18:25 +0530

    Time: 1 ms (37.22%) | Memory: 28.2 MB (28.24%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,36 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt=0;
-        int ele;
-    
-        for(int i=0;i<nums.size();i++){
-            if(cnt==0){
-            cnt=1;
-            ele=nums[i];
-            }
-            else if(nums[i]==ele){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
-        }
-        int cnt1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==ele){
-                cnt1++;
-            }
-        }
-            if(cnt1>nums.size()/2){
-                return ele;
-            }
-            else{
-                return -1;
-            }
-        
-        
-
+        sort(nums.begin(),nums.end());
+       return nums[nums.size()/2];
     }
-    
 };
