b5d28bccf209367950e82b809cb2776faf723609
Ayushi Barapatre <163396909+Onelastcode05@users.noreply.github.com>
2025-09-25 23:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.49%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+        unordered_map<char,char> mapST, mapTS;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char cs = s[i], ct = t[i];
+            if (mapST.count(cs) && mapST[cs] != ct) return false;
+            if (mapTS.count(ct) && mapTS[ct] != cs) return false;
+            mapST[cs] = ct;
+            mapTS[ct] = cs;
+        }
+        return true;
+    }
+};
