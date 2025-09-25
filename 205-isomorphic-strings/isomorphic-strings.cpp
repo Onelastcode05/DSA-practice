244cf7725a5a9601086456085f078b5e0927cfe4
Ayushi Barapatre <163396909+Onelastcode05@users.noreply.github.com>
2025-09-26 00:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (56.49%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
-        unordered_map<char,char> mapST, mapTS;
-        for (size_t i = 0; i < s.size(); ++i) {
-            char cs = s[i], ct = t[i];
-            if (mapST.count(cs) && mapST[cs] != ct) return false;
-            if (mapTS.count(ct) && mapTS[ct] != cs) return false;
-            mapST[cs] = ct;
-            mapTS[ct] = cs;
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int>lastS(256,0);
+        vector<int> lastT(256,0);
+        for(int i=0;i<s.size();i++){
+            if(lastS[s[i]]!=lastT[t[i]]){
+                return false;
+            }
+            lastS[s[i]]=i+1;
+            lastT[t[i]]=i+1;
         }
         return true;
     }
