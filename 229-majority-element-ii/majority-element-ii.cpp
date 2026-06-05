fd842838afdd369f492d9411eb71f02e197baf3b
Ayushi Barapatre <163396909+Onelastcode05@users.noreply.github.com>
2026-06-05 19:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (44.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -5,20 +5,21 @@ public:
         int el2=INT_MIN;
         int cnt1=0;
         int cnt2=0;
+        vector<int>ans;
         int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            if(cnt1==0 && el2!=nums[i]){
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && nums[i]!=el2){
                 el1=nums[i];
-                cnt1++;
+                cnt1=1;
             }
-            else if(cnt2==0 && el1!=nums[i]){
+            else if(cnt2==0 && nums[i]!=el1){
                 el2=nums[i];
-                cnt2++;
+                cnt2=1;
             }
-            else if(el1==nums[i]){
+            else if(nums[i]==el1 ){
                 cnt1++;
             }
-            else if(el2==nums[i]){
+            else if(nums[i]==el2 ){
                 cnt2++;
             }
             else{
@@ -26,21 +27,18 @@ public:
                 cnt2--;
             }
         }
-        
          cnt1=0;
          cnt2=0;
-        for(int i=0;i<n;i++){
-            if(el1==nums[i]){
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el1){
                 cnt1++;
-            }else if(el2==nums[i]){
+            }
+            if(nums[i]==el2){
                 cnt2++;
             }
-
         }
-        vector<int>mylist;
-        int mini=n/3;
-        if(cnt1>mini) mylist.push_back(el1) ;
-        if(cnt2>mini) mylist.push_back(el2);
-        return mylist;
+        if(cnt1>n/3){ans.push_back(el1);}
+        if(cnt2>n/3){ans.push_back(el2);}
+        return ans;
     }
 };
