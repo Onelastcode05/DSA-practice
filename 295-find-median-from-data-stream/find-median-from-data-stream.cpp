f0a129090e17c516b09b71ec5649c1ebf1e7d34b
Ayushi Barapatre <163396909+Onelastcode05@users.noreply.github.com>
2026-06-23 23:25:40 +0530

    Time: 95 ms (27.32%) | Memory: 148.6 MB (63.67%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,29 @@
+class MedianFinder {
+public:
+    priority_queue<int> small; 
+    priority_queue<int, vector<int>, greater<int>> large; 
+
+    MedianFinder() {
+        
+    }
+
+    void addNum(int num) {
+        small.push(num);
+
+        large.push(small.top());
+        small.pop();
+
+        if (large.size() > small.size()) {
+            small.push(large.top());
+            large.pop();
+        }
+    }
+
+    double findMedian() {
+        if (small.size() > large.size()) {
+            return small.top();
+        }
+
+        return (small.top() + large.top()) / 2.0;
+    }
+};
